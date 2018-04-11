cd7a668b53d9814249df5d8c4e255b1e5aa72e8e
Parthiv P <parthiv.parikh95@gmail.com>
2018-04-10 20:51:46 -0400

    Working with tuned parameters

--- cartPole.py
+++ cartPole.py
@@ -7,6 +7,8 @@ import gym
 import numpy as np
 
 BUCKETS = (1,1,6,3)	#constrain the continuous feature space to discrete values
+MIN_EPSILON = 0.001
+MIN_ALPHA = 0.01
 
 env = gym.make('CartPole-v0')
 
@@ -42,26 +44,41 @@ def update_Q_table(prev_state, next_state, action, reward, alpha, gamma):
 	#standard formula to update q table
 	Q_table[prev_state][action] += alpha * (reward + gamma * np.max(Q_table[next_state]) - Q_table[prev_state][action])
 
-def simulate(N_EPISODES, MAX_STEPS, epsilon, alpha, gamma):
-	for episode in range(N_EPISODES):
+def get_explore_rate(t):
+    return max(MIN_EPSILON, min(0.8, 1.0 - np.log((t+1)/10)))
 
+def get_learning_rate(t):
+    return max(MIN_ALPHA, min(0.5, 1.0 - np.log((t+1)/10)))
+
+def simulate(N_EPISODES, MAX_STEPS, gamma):
+	epsilon = get_explore_rate(0)
+	alpha = get_learning_rate(0)
+
+	for episode in range(N_EPISODES):
 		init_state = map_state(env.reset())
 		state = init_state
+
 		for step in range(MAX_STEPS):
 			env.render()
+
 			action = choose_action(state, epsilon)
 			observation, reward, done, info = env.step(action)
 			next_state = map_state(observation)
 			update_Q_table(state, next_state, action, reward, alpha, gamma)
 			state = next_state
+
+			
 			if done:
 				print("Episode finished after {} timesteps".format(step+1))
 				break
-
+		if (True):
+				print ("Epi: {} Step: {} epsilon: {} alpha: {}"
+					.format(episode, step, epsilon, alpha))
+		#update learning and explore rate
+		epsilon = get_explore_rate(episode)
+		alpha = get_learning_rate(episode)
 
 if __name__ == '__main__':
-	simulate(N_EPISODES = 100,
+	simulate(N_EPISODES = 300,
 			MAX_STEPS = 200,
-			epsilon = 0.9,
-			alpha = 0.5,
-			gamma = 0.9)
+			gamma = 0.99) #gamma 0.99 is very important anything less affects the behavior drastically
